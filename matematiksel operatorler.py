3f9cd546af8c8910eb3a8baba07ba7c08a970ddc
Kullemci <114801693+Kullemci@users.noreply.github.com>
2022-10-08 11:58:46 +0300

    matematiksel operatörler

---
+++ matematiksel operatorler.py
@@ -0,0 +1,24 @@
+"""
+Toplama: +
+Çıkarma: -
+Çarpma: *
+Bölme: /
+Üs: **
+Mod: %
+Tam Bölme: //
+"""
+#toplama
+print(5432+9241)
+#çıkarma
+print(5678-1234)
+#çarpma
+print(98*45)
+#bölme
+print(4567/9812)
+#üs alma
+print(3**4)
+print(2**5**4)
+#mod almaa
+print(40%3)
+#tam bölme
+print(40//3  )
