1cfaf242ff2045d2efd2912599e8eaffce74474d
Kullemci <114801693+Kullemci@users.noreply.github.com>
2022-10-15 11:30:45 +0300

    Create karakter_dizileri.py

---
+++ karakter_dizileri.py
@@ -0,0 +1,16 @@
+a=("Muhsin")
+b=(15)
+
+print("Adınız:"+ a + ", Yaşınız:"+str(b))
+
+
+#karakter verisinin uzunluğuna bakalım
+print(len(a))
+
+#str veri tipindeki değerin çeşitli elemanlarını yazdırakım
+#elemanlar 0'dan başlaar
+print(a[0])
+print(a[1])
+print(a[2])
+print(a[3])
+
