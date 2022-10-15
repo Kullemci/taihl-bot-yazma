ddf5f305f6d07d310f59b47fbfbbb1fa6a2bc35d
Kullemci <114801693+Kullemci@users.noreply.github.com>
2022-10-15 11:04:33 +0300

    veri tipi dönüşümleri

--- Veri_tipi_donusumleri.py
+++ Veri_tipi_donusumleri.py
@@ -1,4 +1,40 @@
 # Kullanıcıdan 2 sayı alıp toplama işlemi yapan program
-a=float(input("Sayı girin:"))
-b=float(input("sayı girin:"))
-print(a+b)
+#a=float(input("Sayı girin:"))
+#b=float(input("sayı girin:"))
+#print(a+b)
+
+# Farklı veri tiplerine bakalım:type()
+x=1 #"nteger
+y=1.2 # float
+ad="Muhsin" #string
+sinav_basarili_Mi= True #boolean
+
+
+print(type(x))
+print(type(y))
+print(type(ad))
+print(type(sinav_basarili_Mi))
+
+#tip dönüştürme
+
+#int den floata
+print(float(x))
+
+#floatdan inte
+print(int(y))
+
+#booldan str ye
+print(sinav_basarili_Mi)#bool olarak true
+print(str(sinav_basarili_Mi)) #str olarak true
+
+
+#bool dan inte
+print(int(sinav_basarili_Mi))
+
+#int den str ye
+print(str(x))#str olark
+
+#strden inte
+
+print(int(ad))
+print(ad) #metin inte dönüşmez
