b66215fe932125b96fe9e55ffb707e8bae73c7d1
Kullemci <114801693+Kullemci@users.noreply.github.com>
2022-10-15 09:56:24 +0300

    değişken tanımlama dersi bitti

---
+++ degisken_tanımlama.py
@@ -0,0 +1,46 @@
+# Değişkenler olmadan 2 öğrencinin sınav notlarını hesaplayalım
+# print( (80*0.4) + (75*0.6) )
+# print( (45*0.4) + (60*0.6) )
+
+# değişkenlerle 2 öğrencinin sınav notlarını hesaplayalım
+sinav1_yuzde=0.4
+sinav2_yuzde=0.6
+
+
+#print( (80*sinav1_yuzde) + (75*sinav2_yuzde) )
+#print( (45*sinav1_yuzde) + (60*sinav2_yuzde) )
+
+#DEĞİŞKEN TANIMLAMA KURALLARI
+#RAKAM İLE BAŞLAYAMAZ
+
+#1sayi= 85 hata verdi
+
+sayi1=85
+print(sayi1)
+
+#Büyük küçük harflere duyarlıdır
+
+number=12
+NUMBER=13
+print(number)
+print(NUMBER)
+
+#Türkçe karakter kullanılmaz
+#yaş=18 hatalı kullanım
+yas=18 #Türkçe karakter olmadan türkçe yazmak(tavsiy edilmez)
+age=17
+print(yas)
+print(age)
+
+
+#Değişkenlerdeki verileri birlleştirme
+x=1 #integer
+y=1.2 # float
+ad="Muhsin" #string
+sinav_basarili_Mi= True #boolean
+
+print(x+y)
+#print(x+ad) hata verdi
+
+print(x+sinav_basarili_Mi)
+#print(ad+sinav_basarili_Mi) hata verdi
