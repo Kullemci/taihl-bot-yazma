86878da51d53ffdaba241376b874fbff09bfa395
Kullemci <114801693+Kullemci@users.noreply.github.com>
2022-11-26 11:29:27 +0300

    Create karsilastırma operatorleri.py

---
+++ karsilastırma operatorleri.py
@@ -0,0 +1,43 @@
+"""
+KARŞILAŞTIRMA OPERATÖRLERİ
+//////////////////////////////////////////////////////////////////////////////
+Python'da değişkenlerin içindeki verileri karşılaştırmak için kullanırız
++----------+----------------+-----------+
+| Operatör | Açıklama       | Kullanımı |
++----------+----------------+-----------+
+|    ==    | eşit mi?       |   x == y  |
++----------+----------------+-----------+
+|    !=    | eşit değil mi? |   x != y  |
++----------+----------------+-----------+
+|     >    | büyük mü?      |   x > y   |
++----------+----------------+-----------+
+|     <    | küçük mü?      |   x < y   |
++----------+----------------+-----------+
+|    >=    | büyük eşit mi? |   x >= y  |
++----------+----------------+-----------+
+|    <=    | küçük eşit mi? |   x <= y  |
++----------+----------------+-----------+
+"""
+
+#veri tabanı
+
+#kullanici_adi= " Emre"
+#Sifre="1234"
+
+#k_kadi=input("Kullanıcı adını yazın:")
+#k_sifre=input("Şifrenizi girin:")
+#print(f"Kullanıcı adı doğrumu:{kullanici_adi==k_kadi}")
+#print(f"Şİfre  doğrumu:{Sifre==k_sifre}")
+
+
+#kullanıcıdan 2 sayı al,birbirine "eşit değil mi" kontrolünü yap
+#u=int(input("rakam giriniz"))
+#e=int(input("rakam giriniz"))
+#print(f"Eşit değil mi:{sayi1 != sayi2}")
+
+
+# iki kişinin yaşını karşılaştıralım (büyük mü? küçük mü?)
+yas1=36
+yaas2=25
+print(f"yas1 büyük mü küçük mü: {yas1 > yaas2}")
+print(f"yas2 büyük mü küçük mü: {yaas2 > yas1}")
