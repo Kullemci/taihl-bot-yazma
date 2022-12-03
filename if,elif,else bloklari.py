d936e68ba563eab837078cb51e2a06b2fc5b4fbd
Kullemci <114801693+Kullemci@users.noreply.github.com>
2022-12-03 12:01:28 +0300

    Create if,elif,else bloklari.py

---
+++ if,elif,else bloklari.py
@@ -0,0 +1,11 @@
+sayi1=int(input("Bir rakam giriniz:"))
+sayi2=int(input("Bir rakam giriniz:"))
+
+if(sayi1>sayi2):
+    print("sayi1,sayi2'den büyüktür")
+
+elif(sayi1==sayi2):
+    print("sayi1 ile sayi2 eşittir")
+
+else:
+    print("sayi2,sayi1'den büyüktür")
