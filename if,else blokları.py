7a10cb01f86a15f27a1ba5c9058af7a187d4ee15
Kullemci <114801693+Kullemci@users.noreply.github.com>
2022-12-03 12:01:25 +0300

    Create if,else blokları.py

---
+++ if,else blokları.py
@@ -0,0 +1,8 @@
+sayi1=int(input("Bir rakam giriniz:"))
+sayi2=int(input("Bir rakam giriniz:"))
+
+if(sayi1>sayi2):
+    print("sayi1,sayi2'den büyüktür")
+
+else:
+    print("sayi2,sayi1'den büyüktür")
